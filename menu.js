e3f31fbc3ce13ba6a6368c6a65fbaf24bd3b239a
Marek Augustynowicz <marek.augustynowicz@verkstedt.com>
2024-01-17 16:19:28 +0100

    fetch only one question

--- menu.js
+++ menu.js
@@ -1,60 +1,48 @@
+let first = true;
+
 /* global AFRAME */
 import { fetchQuestionsFromOpentdb } from "./fetchQuestionsFromOpentdb.js";
 AFRAME.registerComponent("menu", {
   init: async function () {
-    // var data = {
-    //   question: "In the server hosting industry IaaS stands for ... ?",
-    //   answers: [
-    //     {
-    //       text: "Infrastructure as a Service",
-    //       value: true,
-    //     },
-    //     {
-    //       text: "Internet as a Service",
-    //       value: false,
-    //     },
-    //     {
-    //       text: "Infrastructure and a Server",
-    //       value: false,
-    //     },
-    //     {
-    //       text: "Infrastructure as a Server",
-    //       value: false,
-    //     },
-    //   ],
-    // };
-    var data = (await fetchQuestionsFromOpentdb())[0]; // Array
-    console.log(data);
-    var answers = [];
-    answers.push(data.correct_answer);
-    answers.push(...data.incorrect_answers);
-
-    answers.sort(() => Math.random() - 0.5);
-
     var el = this.el;
 
-    this.questionEl = this.el.querySelector("[data-question]");
-    this.questionEl.setAttribute("button", {
-      label: data.question,
-      width: 0.5,
-      height: 0.1,
-    });
 
-    this.answersEl = this.questionEl.querySelector("[data-answers]");
-    this.answerEls = this.el.querySelectorAll("[data-answer]");
-    var k = 0;
-    Array.from(this.answerEls).forEach((answerEl) => {
-      answerEl.setAttribute("button", {
-        label: answers[k],
+    if (first) {
+      first = false
+      var data = (await fetchQuestionsFromOpentdb())[0]; // Array
+      console.log(data);
+      var answers = [];
+      answers.push(data.correct_answer);
+      answers.push(...data.incorrect_answers);
+
+      answers.sort(() => Math.random() - 0.5);
+
+      this.questionEl = this.el.querySelector("[data-question]");
+      this.questionEl.setAttribute("button", {
+        label: data.question,
         width: 0.5,
         height: 0.1,
       });
-      answerEl.setAttribute(
-        "data-answer",
-        answers[k] === data.correct_answer ? "correct" : "incorrect"
-      );
-      k++;
-    });
+
+      this.answersEl = this.questionEl.querySelector("[data-answers]");
+      this.answerEls = this.el.querySelectorAll("[data-answer]");
+      var k = 0;
+      Array.from(this.answerEls).forEach((answerEl) => {
+        answerEl.setAttribute("button", {
+          label: answers[k],
+          width: 0.5,
+          height: 0.1,
+        });
+        answerEl.setAttribute(
+          "data-answer",
+          answers[k] === data.correct_answer ? "correct" : "incorrect"
+        );
+        k++;
+      });
+  }
+
+
+
     var menuBackGroundEl = document.createElement("a-entity");
     menuBackGroundEl.setAttribute("geometry", {
       primitive: "box",
